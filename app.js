6b052c810817b7d538eb307c4aea6e140f6d5d86
doniwirawan <doniwirawan166@gmail.com>
2022-05-12 14:33:58 +0800

    make the code cleaner

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const line = require('@line/bot-sdk')
 const client = new line.Client(configuration)
 
 app.get('/', (req, res) => {
-    res.status(200).send('Chatbot Tutorial')
+    res.status(200).send('Line Chatbot Running')
 })
 
 
@@ -19,12 +19,12 @@ app.post('/event', line.middleware(configuration),(req,res) => {
         // command list
         client.replyMessage(event.replyToken, { type: 'text', text: 'Your command list: /command, /second, /creator' }, false)
 
-
         // assign your command and text response here
         if(event.message.text.toLowerCase().includes('/command')){
             client.replyMessage(event.replyToken, { type: 'text', text: 'Your command get trigger here' }, false)
 
         }
+
         // second command
         if (event.message.text.toLowerCase().includes('/second')){
             client.replyMessage(event.replyToken, { type: 'text', text: 'Your second command trigger here' }, false)
@@ -38,7 +38,7 @@ app.post('/event', line.middleware(configuration),(req,res) => {
         }
     })
 
-    res.status(200).send('chatbot tutorial')
+    res.status(200).send('Line Chatbot Running')
 })
 
 
