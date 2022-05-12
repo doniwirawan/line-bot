91c16f7dc9d39ddeed4b918342fa2b188b8f57d2
doniwirawan <doniwirawan166@gmail.com>
2022-05-12 13:39:17 +0800

    line bot

---
+++ app.js
@@ -0,0 +1,26 @@
+require('dotenv').config()
+const configuration = {
+    channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
+    channelSecret: process.env.LINE_CHANNEL_SECRET
+}
+
+const express = require('express')
+const app = express()
+const line = require('@line/bot-sdk')
+const client = new line.Client(configuration)
+
+app.get('/', (req, res) => {
+    res.status(200).send('Chatbot Tutorial')
+})
+
+
+app.post('/event', line.middleware,(req,res) => {
+    req.body.events.map(event => {
+        client.replyMessage(event.replyToken, {type: 'text', text:'Hello!'}, false)
+    })
+
+    res.status(200).send('chatbot tutorial')
+})
+
+
+app.listen(process.env.PORT)
